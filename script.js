175b95b451174d028bae8904a839a9a8570918af
Mithilesh27 <144110616+Mithilesh27@users.noreply.github.com>
2023-09-28 13:55:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //initialize variables
 let songIndex=0;
-let audioElement = new Audio('songs/1.mp3');
+let audioElement = new Audio('2.mp3');
 let masterPlay=document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressbar');
 let songItems= Array.from(document.getElementsByClassName('songItem'));
